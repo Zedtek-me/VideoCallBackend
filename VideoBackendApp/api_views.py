8f82ad13a5573355015a240abcc3c50fbd3b735e
unknown <zechariahadebayo42@gmail.com>
2022-06-24 14:40:13 +0100

    api changes

--- VideoBackendApp/api_views.py
+++ VideoBackendApp/api_views.py
@@ -3,12 +3,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
+from .serializers import UserSerializer
 # Create your views here.
 
 
-@api_view
+@api_view(['GET', 'POST'])
 def get_user_details(request, pk): #to get the user details for frontend--> expects POST request
-    try:
-        user= User.videocon.get(pk=pk)
-    except User.DoesNotExist:
-        return Response({'no user': 'the user doesn\'t exist'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        try:
+            user= User.videocon.get(pk=pk)
+            serialized_user= UserSerializer(user)
+            return Response(serialized_user.data, status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'no user': 'the user doesn\'t exist'}, status=status.HTTP_404_NOT_FOUND)
+    print(request.data)
+    data= UserSerializer(data=request.POST)
+    if data.is_valid():
+        data.save()
+        return Response(data.data, status.HTTP_201_CREATED)
+    else: return Response({'Invalid': 'invalid data given'})
--- VideoBackendApp/urls.py
+++ VideoBackendApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import api_views
 urlpatterns= [
-    path('authenticate', api_views.get_user_details, name='authenticate')
+    path('<int:pk>', api_views.get_user_details, name='authenticate')
 ]
