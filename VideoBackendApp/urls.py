852f1b6c420da0cc585e8504a35a90ad3df62358
unknown <zechariahadebayo42@gmail.com>
2022-07-01 12:40:32 +0100

    views and others, for backend rendering.

--- VideoBackendApp/urls.py
+++ VideoBackendApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import api_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns= [
     # api views for react authentication
     path('authenticate/', api_views.authen_ticate, name= 'authenticate'),
@@ -10,5 +12,11 @@ urlpatterns= [
     path('', views.home, name='home page'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('settings/', views.setting, name='profile settings'),
-    path('meeting_room/', views.meeting, name='meeting room')
+    path('meeting_room/', views.meeting, name='meeting room'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_ULR, document_root=settings.MEDIA_ROOT)
--- VideoBackendApp/views.py
+++ VideoBackendApp/views.py
@@ -2,22 +2,45 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import User
+from django.contrib.auth.decorators import login_required, permission_required
 
 
-# function views for requests and response
+# functional views for requests and response
 def home(request):
-    # returns the login page
-    return render(request, 'login.html', context={}, content_type='text/html')
+    # returns the dashboard if login successful
+    if request.method == 'POST':
+        data= request.POST
+        user= authenticate(request, **data)
+        if user:
+            login(request, user)
+            messages.success(request, 'welcome %s!' %user)
+            return redirect('dashboard')
+        messages.error(request, 'Email or Password is wrong! Note: they might be case sensitive')
+        return redirect('homoe page')
+    msg= messages.get_messages(request)
+    return render(request, 'login.html', context={'msgs':msg}, content_type='text/html')
 
 def signup(request):
+    if request.method == 'POST':
+        data= request.POST
+        if data.get('password1') == data.get('password2'):#are the two passwords given correct?
+            user= User.videocon.create_user(**data)
+            messages.success(request, 'signup successful! You can login now.')
+            return redirect('login')
+        messages.error(request, 'Your passwords didn\'t match; check them well.')
+        return('signup')
+    # get requests return the signup page
     return render(request, 'signup.html', context={})
 
 def dashboard(request):
-    return render(request, 'dashboard.html', context={})
+    user = request.user
+    return render(request, 'dashboard.html', context={'user': user})
 
 def setting(request):
-    return render(request, 'settings.html', context={}, content_type='text/html')
+    user = request.user
+    return render(request, 'settings.html', context={'user': user}, content_type='text/html')
 
 def meeting(request):
-    return render(request, 'meeting_room.html', context={}, content_type='text/html')
+    user = request.user
+    return render(request, 'meeting_room.html', context={'user': user}, content_type='text/html')
 
