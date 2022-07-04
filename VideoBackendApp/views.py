00046e04ae36e03459ef42a6f7f0e09456742a13
unknown <zechariahadebayo42@gmail.com>
2022-07-04 23:51:29 +0100

    in progress

--- VideoBackendApp/views.py
+++ VideoBackendApp/views.py
@@ -1,10 +1,10 @@
-import email
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .models import User
+from .models import User, Meeting
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db import IntegrityError
+import datetime
 
 
 # functional views for requests and response (not meant to be consumed by the react frontend. Just dedicated, entirely to the host(domain) of this backend app.)
@@ -64,12 +64,21 @@ def setting(request):
     return render(request, 'settings.html', context={'user': user, 'msgs':msgs}, content_type='text/html')
 
 # the meeting room view-> reserver for only loggedin users who have permissions to start a meeting, or have requested to join a meeting
+
+# a utility function that returns an an iterable of scheduled and recent meetings
+def current_and_past_meetings():
+    recent_meetings= Meeting.objects.filter(starting__lt=datetime.datetime.now())
+    upcoming_meetings= Meeting.objects.filter(ending_gt=datetime.datetime.now())
+    return recent_meetings, upcoming_meetings
+
+
 @login_required(login_url='home page')
 @permission_required(['VideoBackendApp.can_start_meeting', 'VideoBackendApp.can_join_meeting'], login_url='home page', raise_exception=True)
 def meeting(request):
+    recent, coming= current_and_past_meetings()
     user = request.user
     msgs=messages.get_messages(request)
-    return render(request, 'meeting_room.html', context={'user': user, 'msgs':msgs}, content_type='text/html')
+    return render(request, 'meeting_room.html', context={'user': user, 'msgs':msgs,'recent': recent, 'current': coming}, content_type='text/html')
 
 def log_out(request):
     user= request.user
