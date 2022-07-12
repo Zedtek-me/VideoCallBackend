db9e5f949ba5d172a6d6f54631bffe128ddb55be
unknown <zechariahadebayo42@gmail.com>
2022-07-12 23:38:00 +0100

    adding node.js webpack for static files management and es6 syntaxes.

---
+++ VideoBackendApp/webpack.config.js
@@ -0,0 +1,16 @@
+const path= require('Path')
+
+module.export={
+    entry: "",
+    output:{
+        filename:"",
+        path:""
+    },
+    module: {
+        rules:[{
+            test: "",
+            exclude:"node_modules",
+            use:""
+        }]
+    }
+}
